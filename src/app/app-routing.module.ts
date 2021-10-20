8aa2b31068f56c595aa3f01da81539495ea689ce
jakru00 <jakru00@gmx.de>
2021-10-20 17:32:37 +0200

    GPF-2 added route for data-management-component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataManagementComponent } from './shared/data-management/data-management.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DataManagementComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
