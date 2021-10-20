83c2a9319d480265a0cc9cb86595db79c50c3811
jakru00 <jakru00@gmx.de>
2021-10-20 17:41:10 +0200

    GPF-2 fixed errors: installed bootstrap and removed import of non-existing login-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DataManagementComponent } from './shared/data-management/data-management.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     DataManagementComponent
   ],
   imports: [
