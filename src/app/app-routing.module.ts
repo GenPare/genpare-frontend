dbd01c92508a1f6c53d9f780378a25462393026b
jakru00 <jakru00@gmx.de>
2021-10-21 16:29:30 +0200

    GPF-2 made use of simpler import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DataManagementComponent } from './shared/data-management/data-management.component';
+import { DataManagementComponent } from '@shared/data-management/data-management.component';
 
 const routes: Routes = [
-  { path: '', component: DataManagementComponent }
+  { path: 'data-management', component: DataManagementComponent },
+  { path: '', redirectTo: 'data-management', pathMatch: 'full' }
 ];
 
 @NgModule({
