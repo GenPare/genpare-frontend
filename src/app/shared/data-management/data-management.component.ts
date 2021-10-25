584c157ef2acc8c36605638406a7f5d4df6fa42a
jakru00 <jakru00@gmx.de>
2021-10-25 13:03:50 +0200

    GPF-2 implemented data management elements without saving all values in component

--- src/app/shared/data-management/data-management.component.ts
+++ src/app/shared/data-management/data-management.component.ts
@@ -7,6 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataManagementComponent implements OnInit {
 
+  federal_states: String[] = [
+    'Baden-Württemberg',
+    'Bayern',
+    'Berlin',
+    'Brandenburg',
+    'Bremen',
+    'Hamburg',
+    'Hessen',
+    'Mecklenburg-Vorpommern',
+    'Niedersachsen',
+    'Nordrhein-Westfalen',
+    'Rheinland-Pfalz',
+    'Saarland',
+    'Sachsen',
+    'Sachsen-Anhalt',
+    'Schleswig-Holstein',
+    'Thüringen'
+  ]
+
+  genders: String[] = [
+    'männlich',
+    'weiblich',
+    'divers'
+  ]
+
+  education_degrees: String[] = [
+    'kein Abschluss',
+    'Hauptschulabschluss',
+    'Realschulschluss',
+    'Abitur / Allgemeine Hochschulreife',
+    'Fachhochschulreife',
+    'Diplom (Hochschule)',
+    'Magister (Hochschule)',
+    'Bachelor (Hochschule)',
+    'Master (Hochschule)',
+    'Doktor / Promotion (Hochschule)',
+    'Geselle',
+    'Meister',
+    'Einfacher Dienst',
+    'Mittlerer Dienst',
+    'Gehobener Dienst',
+    'Höherer Dienst'
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
