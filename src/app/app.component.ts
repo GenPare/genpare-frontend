b59bdf6b24765b65f642695db56cab5e88ef0f01
jakru00 <jakru00@gmx.de>
2021-10-27 17:53:35 +0200

    GPF-3 moved username on navbar to right side

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'genpare';
+  title: String = 'genpare';
+  username: String = "genparer"
 }
