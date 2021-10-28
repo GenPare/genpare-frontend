206c7830aec18f477a8d058cd5b10c5d62246d99
Bruce Weber <bruce.weber@t2informatik.de>
2021-10-28 23:09:18 +0200

    GPF-1 discarded own components for auth0 authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,31 @@ import { NgModule } from '@angular/core';
 import { DatePipe } from "@angular/common";
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DataManagementComponent } from './shared/data-management/data-management.component';
 import { ProfileManagementComponent } from './profile-management/profile-management.component';
-import { RegisterComponent } from './components/register/register.component';
+import { AuthModule } from '@auth0/auth0-angular';
+import { StartPageComponent } from './components/start-page/start-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataManagementComponent,
     ProfileManagementComponent,
-    LoginComponent,
-    RegisterComponent
+    StartPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AuthModule.forRoot({
+      domain: 'dev-t9d7tc1e.eu.auth0.com',
+      clientId: 'OLtqeuEcVpFPKkLIRaKJ3ENrv3hUUlWY'
+    })
   ],
   providers: [DatePipe],
   bootstrap: [AppComponent]
