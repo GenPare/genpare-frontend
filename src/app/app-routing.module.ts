47dfca04d5de3a64316cfabdbb44b472a08cbbf5
jakru00 <jakru00@gmx.de>
2021-10-30 16:55:42 +0200

    GPF-5 added authguard to compare-route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { StartPageComponent } from '@comp/start-page/start-page.component';
 import { ProfileManagementComponent } from "@comp/profile-management/profile-management.component";
 
 const routes: Routes = [
-  { path: 'compare', component: CompareComponent },
+  { path: 'compare', component: CompareComponent, canActivate: [AuthGuard] },
   { path: 'profile', component: ProfileManagementComponent, canActivate: [AuthGuard]},
   { path: 'start', component: StartPageComponent },
   { path: '', redirectTo: 'compare', pathMatch: 'full' }
