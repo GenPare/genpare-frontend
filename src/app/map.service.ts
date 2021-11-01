d9c1c58b816c530fe83d61dc470d3b72337373c6
jakru00 <jakru00@gmx.de>
2021-11-01 21:34:40 +0100

    GPF-17 implemented initilize method to fill maps

--- src/app/map.service.ts
+++ src/app/map.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { education_degrees_s, federal_states_s, genders_s } from "@shared/model/select_data";
+import { education_degrees_b, federal_states_b, genders_b } from "@shared/model/backend_data";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapService {
 
-  constructor() { }
+  gendersMap = new Map();
+  federalStatesMap = new Map();
+  educationDegreesMap = new Map();
+
+  constructor() {
+    this.initialize();
+  }
+
+  initialize(): void {
+    for(let i=0; i<federal_states_s.length; i++){
+      this.educationDegreesMap.set(education_degrees_s[i], education_degrees_b[i]);
+    }
+
+    for(let i=0; i<federal_states_s.length; i++){
+      this.federalStatesMap.set(federal_states_s[i], federal_states_b[i]);
+    }
+
+    for(let i=0; i<genders_s.length; i++){
+      this.gendersMap.set(genders_s[i], genders_b[i]);
+    }
+  }
 }
