29d723527b1687e2fcf7ef972a465612eeb79792
Bruce Weber <bruce.weber@t2informatik.de>
2021-11-02 21:07:38 +0100

    GPF-8 move merged data.data-management.component

--- src/app/components/data-management/data-management.component.ts
+++ src/app/components/data-management/data-management.component.ts
@@ -1,6 +1,17 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  education_degrees_f,
+  federal_states_f,
+  genders_f,
+} from '@shared/model/frontend_data';
+import {
+  DataManagementService,
+  ProfileData,
+} from 'app/data-management.service';
 import { MemberService } from 'app/services/member.service';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { education_degrees, federal_states, genders } from './select-options';
 
 @Component({
@@ -19,6 +30,13 @@ export class DataManagementComponent {
   federal_states = federal_states;
   genders = genders;
 
+  federal_state?: string;
+  salary?: number;
+  education_degree?: string;
+  job_title?: string;
+
+  existingData$: Observable<ProfileData>;
+
   data_management = new FormGroup({
     job_title: new FormControl('', Validators.required),
     education_degree: new FormControl('', Validators.required),
@@ -28,7 +46,25 @@ export class DataManagementComponent {
     age: new FormControl('', Validators.required),
   });
 
-  constructor(private memberService: MemberService) {}
+  constructor(
+    private memberService: MemberService,
+    private dataManagementService: DataManagementService
+  ) {
+    this.federal_states = federal_states_f;
+    this.genders = genders_f;
+    this.education_degrees = education_degrees_f;
+    this.existingData$ = this.dataManagementService.getProfileData();
+    this.existingData$
+      .pipe(
+        tap((data) => {
+          (this.federal_state = data.federal_state),
+            (this.salary = data.salary),
+            (this.education_degree = data.education_degree),
+            (this.job_title = data.job_title);
+        })
+      )
+      .subscribe();
+  }
 
   save(): void {
     if (!this.memberService.sessionID$) {
@@ -39,6 +75,13 @@ export class DataManagementComponent {
           this.data_management.value.gender
         );
         this.registeredEmitter.emit(true);
+        const newData = {
+          job_title: this.data_management.value.job,
+          salary: this.data_management.value.salary,
+          education_degree: this.data_management.value.education_degree,
+          federal_state: this.data_management.value.federal_state,
+        };
+        this.dataManagementService.newProfileData(newData);
       }
     }
   }
