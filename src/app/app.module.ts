80caf2fb6581dd018d1664d504985550678b7b6f
Bruce Weber <bruce.weber@t2informatik.de>
2022-02-10 16:18:45 +0100

    GPF-25 added TokenInjection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule , DatePipe } from "@angular/common";
+import { CommonModule, DatePipe } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { MDBBootstrapModule } from "angular-bootstrap-md";
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { StartPageComponent } from '@comp/start-page/start-page.component';
 import { CompareComponent } from '@comp/compare/compare.component';
 import { ProfileManagementComponent } from '@comp/profile-management/profile-management.component';
@@ -17,9 +17,13 @@ import { DataManagementComponent } from '@comp/data-management/data-management.c
 import { AuthModule } from '@auth0/auth0-angular';
 import { environment as env } from 'environments/environment.dev';
 import { LoginSuccessComponent } from './components/login-success/login-success.component';
+import { JwtModule } from '@auth0/angular-jwt';
 
-export const backendURL = "http://localhost:8080"
+export const backendURL = 'http://localhost:8080';
 
+export function tokenGetter() {
+  return sessionStorage.getItem('sessionId');
+}
 
 export class App {
   constructor() {}
@@ -34,7 +38,7 @@ export class App {
     StartPageComponent,
     ToastsContainerComponent,
     SupportPageComponent,
-    LoginSuccessComponent
+    LoginSuccessComponent,
   ],
   imports: [
     BrowserModule,
@@ -43,13 +47,19 @@ export class App {
     FormsModule,
     ReactiveFormsModule,
     AuthModule.forRoot({
-      ...env.auth
+      ...env.auth,
     }),
     MDBBootstrapModule.forRoot(),
     HttpClientModule,
-    CommonModule
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        allowedDomains: ['localhost']
+      },
+    }),
+    CommonModule,
   ],
   providers: [DatePipe, HasEnteredDataGuard],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
